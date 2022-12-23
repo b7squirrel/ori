221d21c964e62d9126a1cf24fca11ea3ea95bd7c
sangyeong <b7squirrel@gmail.com>
2022-12-23 08:54:00 -0800

    HitRoll의 슬롯 애니메이션 했음.

--- Assets/Scripts/Pan/PanInputHandler.cs
+++ Assets/Scripts/Pan/PanInputHandler.cs
@@ -13,6 +13,8 @@ public class PanInputHandler : MonoBehaviour
     {
         CheckCaptureInputHoldTime();
         Capture();
+        CheckHitRollInputHoldTime();
+        HitRoll();
     }
 
     public void Capture()
