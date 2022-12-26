8c0ee13a47bbd2b03cc1e21e23e87eda8212ff6d
sangyeong <b7squirrel@gmail.com>
2022-12-26 14:43:32 -0800

    사용하지 않게 된 Flavour 관련 스크립트들 삭제
    - 롤을 다시 회수하면 남은 lifeTime을 전달하게 해야 함.
    - 롤이 팬 위에서 소멸하면 pullRolls를 해 줘서 빈 슬롯을 채워줘야 함.

--- Assets/Scripts/Roll/FlavourGen.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using UnityEngine;
-
-public class FlavourGen : MonoBehaviour
-{
-    [field : SerializeField]
-    public Flavour.flavourType FlavourType { get; private set; }
-}
--- Assets/Scripts/Roll/IFlavourAction.cs
+++
@@ -1,4 +0,0 @@
-public interface IFlavourAction
-{
-    void FlavourAction();
-}
